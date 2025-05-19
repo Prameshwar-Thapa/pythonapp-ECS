84febe55b27fbf1bc0cb95dd2a5382549ca2ad91
PrameshwarThapa <thapaprameshwar7@gmail.com>
2025-05-19 10:47:09 +1000

    v2

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from ECS via Docker Hub!"
+    return "New version"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
